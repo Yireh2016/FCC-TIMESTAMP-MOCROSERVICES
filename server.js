3b2ca70f99e4dfa4454cffdaaba50a29b357e302
Glitch (fcc-timestamp-microservice-r) <none>
2019-12-22 04:05:58 +0000

    fcc test completed

--- server.js
+++ server.js
@@ -24,18 +24,65 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-app.get("/api/timestamp/:date_string",(req, res)=>{
-  let {date_string}=req.params;
-      console.log("date_string ", date_string)
+const timestampController=(req,res)=>{
+  console.log("req.params ", req.params)
 
-  if(/[0-9]*(=?!-)/.test(date_string)){
+  let {date_string}=req.params;
+  let date;
+  const regexRes=/^[0-9]*$/.test(date_string.toString())
+  console.log("regexRes ", regexRes)
+
+  if(regexRes){
     date_string=parseInt(date_string,10)
+    console.log("date_string ", date_string)
   }
 
-  const date=date_string?new Date(date_string):new Date();
-  const unix=date.getTime() / 1000
-  res.json({unix, utc:date.toUTCString(date)})
-})
+  if(date_string==="void"){
+    date=new Date()
+  }else{
+    date=new Date(date_string);
+  }  
+  
+  
+  if(date.toUTCString(date) === 'Invalid Date'){
+    res.json({error:'Invalid Date'})
+  }else{
+    let unix=date.getTime() ///1000
+  let result={unix, utc:date.toUTCString(date)}
+  console.log("res",result)
+  res.json(result)
+  }
+  
+}
+
+
+app.get("/api/timestamp/:date_string",timestampController)
+
+app.get("/api/timestamp/",(req, res,next)=>{
+  
+if(req.params.hasOwnProperty('date_string')){
+    console.log("req.params",req.params)
+
+ next();
+
+}else{
+    let date_string=new Date()
+    //Math.round((new Date()).getTime() / 1000)
+  let timestamp=date_string.getTime()  //Math.round(date_string.getTime() / 1000)
+      console.log("timestamp ", timestamp)
+  req.params.date_string=timestamp
+  next();
+}
+ 
+  
+},timestampController)
+
+
+
+
+// app.get("/api/timestamp/:date_string",(req, res)=>{
+        
+// })
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
